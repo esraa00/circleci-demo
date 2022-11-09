0aef870cd6ae47856354d66bf35d27bba0dc65fa
esraa00 <em4728644@gmail.com>
2022-11-09 21:35:43 +0200

    circleci was modified

--- sum.js
+++ sum.js
@@ -1,7 +1,7 @@
 const sum = (num1, num2) => {
   return num1 + num2;
+};
 
-
-console.log(sum(`the sum of 10 and 2 is ${sum(10, 2)}`));
+console.log(`the sum of 10 and 2 is ${sum(10, 2)}`);
 
 module.exports = sum;
